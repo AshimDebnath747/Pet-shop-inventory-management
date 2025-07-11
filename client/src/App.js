cdaba95e6bb0c9cdbe2fa3b6fac6301aea20261c
Ashim Debnath <ashimdebnath6767@gmail.com>
2025-07-11 20:21:14 +0530

    Update App.js
    Added a loading animation during the checking of authentication.

--- client/src/App.js
+++ client/src/App.js
@@ -456,9 +456,9 @@ function App() {
 
   if (checkingAuth) {
     return (
-      <div className="flex items-center justify-center min-h-screen">
-        <span>Loading...</span>
-      </div>
+      <div className="flex items-center justify-center h-screen">
+            <div className="animate-spin rounded-full h-16 w-16 border-b-2 border-blue-500"></div>
+        </div>
     );
   }
 
